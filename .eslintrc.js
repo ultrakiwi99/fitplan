1b86c24e90f7760b3a019164342dc6f4a63aaa90
Alex Kruzhkov <ultrakiwi99@gmail.com>
2019-12-24 22:13:33 +0300

    setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,26 +3,31 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
+  parser: "vue-eslint-parser",
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
+  plugins: ["vue"],
+  extends: [
+    "eslint:recommended",
+    "plugin:vue-recommended",
+    "prettier",
+    "prettier/vue"
+  ],
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
       ],
       env: {
         jest: true
       }
     }
   ]
-}
+};
