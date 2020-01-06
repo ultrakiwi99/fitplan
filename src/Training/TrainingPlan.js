1d701c8e0d9329d24ddae60ab9dfb684bcf45819
Alex Kruzhkov <ultrakiwi99@gmail.com>
2020-01-06 22:56:31 +0300

    training plans tests and realisation

--- src/Training/TrainingPlan.js
+++ src/Training/TrainingPlan.js
@@ -1,7 +1,53 @@
+/* eslint-disable no-undef */
 export default class TrainingPlan {
-    addToPlan() { },
-    removeFromPlan() {},
-    save() { },
-    clear() {},
-    next() { },
+    storageName = "fit-app";
+    addToPlan(training) {
+        const saved = this.getPlan();
+        saved.trainings.push(training);
+        this.savePlan(saved);
+    }
+    removeFromPlan(trainingName) {
+        const saved = this.getPlan();
+        saved.trainings = saved.trainings.filter(
+            training => training.name !== trainingName
+        );
+        this.savePlan(saved);
+    }
+    saveTraining(training) {
+        const saved = this.getPlan();
+        saved.saved.push(training);
+        this.savePlan(saved);
+    }
+    nextTraining() {
+        const saved = this.getPlan();
+        if (saved.saved.length === 0) {
+            console.log(saved);
+            if (saved.trainings.length > 1) {
+                return saved.trainings
+                    .sort((tr1, tr2) => tr1.order < tr2.order)
+                    .pop();
+            }
+            return saved.trainings.pop();
+        }
+    }
+    clearSaved() {
+        const saved = this.getPlan();
+        saved.saved = [];
+        this.savePlan(saved);
+    }
+    getPlan() {
+        let plan = localStorage.getItem(this.storageName);
+        if (!plan) {
+            plan = {
+                trainings: [],
+                saved: []
+            };
+        } else {
+            plan = JSON.parse(plan);
+        }
+        return plan;
+    }
+    savePlan(plan) {
+        localStorage.setItem(this.storageName, JSON.stringify(plan));
+    }
 }
---
+++ tests/unit/TrainingPlan.spec.js
@@ -0,0 +1,145 @@
+import LocalStorageMock from "./mocks/localStrageMock";
+import TrainingPlan from "../../src/Training/TrainingPlan";
+
+global.localStorage = new LocalStorageMock();
+
+describe("TrainingPlan", () => {
+    const plan = new TrainingPlan();
+
+    it("Adds training to training plan", () => {
+        plan.addToPlan({
+            name: "Training A"
+        });
+        plan.addToPlan({
+            name: "Training B"
+        });
+
+        const saved = JSON.parse(global.localStorage.getItem("fit-app"));
+
+        expect(saved["trainings"][0]["name"]).toBe("Training A");
+        expect(saved["trainings"][1]["name"]).toBe("Training B");
+
+        global.localStorage.clear();
+    });
+
+    it("Saves training to saved trainings list", () => {
+        plan.addToPlan({
+            name: "Training A"
+        });
+
+        let saved = JSON.parse(global.localStorage.getItem("fit-app"));
+        expect(saved["trainings"][0]["name"]).toBe("Training A");
+
+        plan.removeFromPlan("Training A");
+
+        saved = JSON.parse(global.localStorage.getItem("fit-app"));
+        expect(saved["trainings"]).toStrictEqual([]);
+
+        global.localStorage.clear();
+    });
+
+    it("Saves training to saved training list", () => {
+        plan.saveTraining({
+            name: "Training A",
+            exersises: [{ name: "exersise 1", sets: [{ reps: 5 }] }]
+        });
+
+        let saved = JSON.parse(global.localStorage.getItem("fit-app"));
+
+        expect(saved["saved"][0]["name"]).toBe("Training A");
+        expect(saved["saved"][0]["exersises"][0]["sets"][0]["reps"]).toBe(5);
+
+        global.localStorage.clear();
+    });
+
+    it("Clears all saved trainings", () => {
+        plan.saveTraining({
+            name: "Training A",
+            exersises: [{ name: "exersise 1", sets: [{ reps: 5 }] }]
+        });
+        plan.saveTraining({
+            name: "Training B",
+            exersises: [{ name: "exersise 2", sets: [{ reps: 5 }] }]
+        });
+
+        plan.clearSaved();
+
+        let saved = JSON.parse(global.localStorage.getItem("fit-app"));
+
+        expect(saved["saved"]).toStrictEqual([]);
+
+        global.localStorage.clear();
+    });
+
+    it("Returns default training if no trainings are saved", () => {
+        plan.addToPlan({
+            name: "Training A",
+            exersises: [
+                {
+                    name: "exersise 1",
+                    maxReps: 5,
+                    weight: 10,
+                    weightProgression: 1.25,
+                    sets: [{ reps: 0 }]
+                }
+            ]
+        });
+
+        expect(plan.nextTraining()).toStrictEqual({
+            name: "Training A",
+            exersises: [
+                {
+                    name: "exersise 1",
+                    maxReps: 5,
+                    weight: 10,
+                    weightProgression: 1.25,
+                    sets: [{ reps: 0 }]
+                }
+            ]
+        });
+        global.localStorage.clear();
+    });
+
+    it("Returns first training by order, if no saved training exist", () => {
+        plan.addToPlan({
+            name: "Training A",
+            order: 0,
+            exersises: [
+                {
+                    name: "exersise 1",
+                    maxReps: 5,
+                    weight: 10,
+                    weightProgression: 1.25,
+                    sets: [{ reps: 0 }]
+                }
+            ]
+        });
+        plan.addToPlan({
+            name: "Training B",
+            order: 1,
+            exersises: [
+                {
+                    name: "exersise 1",
+                    maxReps: 5,
+                    weight: 10,
+                    weightProgression: 1.25,
+                    sets: [{ reps: 0 }]
+                }
+            ]
+        });
+
+        expect(plan.nextTraining()).toStrictEqual({
+            name: "Training A",
+            order: 0,
+            exersises: [
+                {
+                    name: "exersise 1",
+                    maxReps: 5,
+                    weight: 10,
+                    weightProgression: 1.25,
+                    sets: [{ reps: 0 }]
+                }
+            ]
+        });
+    });
+});
