486bfba5e750f9f72c3290ce16824d2e6353f7c9
Alex Kruzhkov <ultrakiwi99@gmail.com>
2020-01-08 17:04:21 +0300

    css tweaks

--- src/views/ExerciseView.vue
+++ src/views/ExerciseView.vue
@@ -1,35 +1,49 @@
 <template>
-  <div>
-    <h2 class="exercise-name">{{ ex.name }}</h2>
-    <SetView
-      v-for="(set, idx) in ex.sets"
-      :set="set"
-      :max-reps="ex.maxReps"
-      :key="idx"
-      :set-idx="idx"
-      @updateReps="updateEx"
-    />
-  </div>
+    <div class="exersise-container">
+        <h3 class="exercise-name">{{ ex.name }}</h3>
+        <div class="buttons-container">
+            <SetView
+                v-for="(set, idx) in ex.sets"
+                :set="set"
+                :max-reps="ex.maxReps"
+                :key="idx"
+                :set-idx="idx"
+                @updateReps="updateEx"
+            />
+        </div>
+    </div>
 </template>
 
 <script>
 import SetView from "./SetView";
 
 export default {
-  name: "ExerciseView",
-  components: { SetView },
-  props: {
-    ex: {
-      type: Object
+    name: "ExerciseView",
+    components: { SetView },
+    props: {
+        ex: {
+            type: Object
+        },
+        exIdx: Number
     },
-    exIdx: Number
-  },
-  methods: {
-    updateEx(setIdx, reps) {
-      this.$emit("updateEx", this.exIdx, setIdx, reps);
+    methods: {
+        updateEx(setIdx, reps) {
+            this.$emit("updateEx", this.exIdx, setIdx, reps);
+        }
     }
-  }
 };
 </script>
 
-<style></style>
+<style>
+.exersise-container {
+    background-color: #f1efec;
+    padding: 0.1rem 0;
+    margin-bottom: 1rem;
+}
+.buttons-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 1rem;
+}
+</style>
--- src/views/SetView.vue
+++ src/views/SetView.vue
@@ -1,28 +1,37 @@
 <template>
-  <div>
     <button class="set-button" @click="updateReps">{{ set.reps }}</button>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "SetView",
-  props: {
-    maxReps: Number,
-    set: Object,
-    setIdx: Number
-  },
-  methods: {
-    updateReps() {
-      const newRepCount = this.set.reps + 1;
-      if (newRepCount > this.maxReps) {
-        this.$emit("updateReps", this.setIdx, 0);
-      } else {
-        this.$emit("updateReps", this.setIdx, this.set.reps + 1);
-      }
+    name: "SetView",
+    props: {
+        maxReps: Number,
+        set: Object,
+        setIdx: Number
+    },
+    methods: {
+        updateReps() {
+            const newRepCount = this.set.reps + 1;
+            if (newRepCount > this.maxReps) {
+                this.$emit("updateReps", this.setIdx, 0);
+            } else {
+                this.$emit("updateReps", this.setIdx, this.set.reps + 1);
+            }
+        }
     }
-  }
 };
 </script>
 
-<style></style>
+<style>
+.set-button {
+    background-color: brown;
+    color: white;
+    border: 1px solid brown;
+    font-size: 1rem;
+    line-height: 1.1rem;
+    padding: 0.3rem 0.5rem;
+    border-radius: 1rem;
+    margin: 0.2rem;
+}
+</style>
