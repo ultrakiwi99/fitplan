e36ec1f724b648f40626d6f9e1f79ab047834b5e
Alex Kruzhkov <ultrakiwi99@gmail.com>
2020-01-14 21:48:08 +0300

    Css for weight-change component.

--- src/views/ExerciseChangeWeight.vue
+++ src/views/ExerciseChangeWeight.vue
@@ -1,14 +1,18 @@
 <template>
     <div class="change-weight">
-        <button class="less-weight" @click="decrease">-</button>
-        <span class="weight">{{ modifiedWeight }}</span>
-        <button class="more-weight" @click="increase">+</button>
-        <button
-            class="save-weight"
-            @click="$emit('updateWeight', modifiedWeight)"
-        >
-            Ok
-        </button>
+        <div class="controls">
+            <button class="less-weight" @click="decrease">-</button>
+            <span class="weight">{{ modifiedWeight }}</span>
+            <button class="more-weight" @click="increase">+</button>
+        </div>
+        <div class="save">
+            <button
+                class="save-weight"
+                @click="$emit('updateWeight', modifiedWeight)"
+            >
+                Ok
+            </button>
+        </div>
     </div>
 </template>
 
@@ -39,4 +43,26 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.change-weight {
+    background-color: #f5dec3;
+    margin-bottom: 1rem;
+}
+.change-weight button {
+    width: 2rem;
+    height: 2rem;
+    border: 0;
+    background-color: brown;
+    color: whitesmoke;
+    border-radius: 0.4rem;
+    margin: 1rem;
+}
+.change-weight .save-weight {
+    background-color: brown;
+    margin-top: 0;
+}
+.weight {
+    font-size: 1.5rem;
+    font-weight: bold;
+}
+</style>
